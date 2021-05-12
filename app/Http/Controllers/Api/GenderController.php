b4c100a488b54c308efdaa66017f10e36ac239f5
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-05-12 10:02:23 -0300

    Implementing models Gender and Category

---
+++ app/Http/Controllers/Api/GenderController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Models\Gender;
+use Illuminate\Http\Request;
+
+class GenderController extends Controller
+{
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    public function index()
+    {
+        return Gender::all();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Gender::create($request->all());
+    }
+
+    public function show(Gender $gender)
+    {
+        //
+    }
+
+    public function edit(Gender $gender)
+    {
+     
+    }
+
+    public function update(Request $request, Gender $gender)
+    {
+        $this->validate($request, $this->rules);
+        $gender->update($request->all());
+        return $gender;
+    }
+
+    public function destroy(Gender $gender)
+    {
+        $gender->delete();
+        return response()->noContent();
+    }
+}
