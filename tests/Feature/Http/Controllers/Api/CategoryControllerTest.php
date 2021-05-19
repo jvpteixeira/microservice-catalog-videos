a360edce117479a3263ce149136fc3b02dba0452
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-05-19 00:21:54 +0000

    Organizing assertion at edition

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -5,11 +5,12 @@ namespace Tests\Feature\Http\Controllers\Api;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use App\Models\Category;
+use Tests\Traits\TestSaves;
 use Tests\Traits\TestValidations;
 
 class CategoryControllerTest extends TestCase
 {  
-    use DatabaseMigrations, TestValidations;
+    use DatabaseMigrations, TestValidations, TestSaves;
 
     private $category;
 
@@ -59,31 +60,20 @@ class CategoryControllerTest extends TestCase
     }
 
     public function testStore(){
-        $response = $this->json('POST',route('categories.store'),[
+        $data = [
             'name' => 'test'
+        ];
+        $response = $this->assertStore($data,$data + ['description' => null, 'is_active' => true, 'deleted_at' => null]);
+        $response->assertJsonStructure([
+            'created_at', 'updated_at'
         ]);
 
-        $id = $response->json('id');
-        $category = Category::find($id);
-
-        $response
-            ->assertStatus(201)
-            ->assertJson($category->toArray());
-        $this->assertTrue($response->json('is_active'));
-        $this->assertNull($response->json('description'));
-
-
-        $response = $this->json('POST',route('categories.store'),[
+        $data = [
             'name' => 'test',
             'description' => 'description',
             'is_active' => false
-        ]);
-
-        $response
-            ->assertJsonFragment([
-                'is_active' => false,
-                'description' => 'description'
-            ]);
+        ];
+        $this->assertStore($data,$data + ['description' => 'description', 'is_active' => false]);
     }
 
     public function testUpdate(){
@@ -154,5 +144,9 @@ class CategoryControllerTest extends TestCase
     protected function routeUpdate()
     {
         return route('categories.update',['category' => $this->category->id]);
+    }
+
+    protected function model(){
+        return Category::class;
     }
 }
