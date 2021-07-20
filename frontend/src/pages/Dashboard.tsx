31c684eee5d1b8490c6017af3d6029e00e894cc4
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-07-19 21:19:50 -0300

    Creating pages structures

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Page } from '../components/Page';
+interface DashBoardProps {
+    
+};
+
+const Dashboard = (props: DashBoardProps) => {
+    return (
+        <Page title='Dashboard'>
+            
+        </Page>
+    );
+};
+
+export default Dashboard;
